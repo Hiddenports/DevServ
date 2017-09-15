74415bf2ed6e19d07bb816a655e70359b2391f72
TUX-PC\Dylan <kernelpanics68@gmail.com>
2017-09-15 23:36:58 +0200

    Zurückgesetzt für C

--- headertest.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include "neuron.h"
-using namespace std;
-int main(void) {
-	cout << "Testing header files..." << endl;
-	neuronal::cell tester;
-}
--- neuron.h
+++ neuron.h
@@ -1,47 +1,12 @@
 #ifndef NEURON_H_
 #define NEURON_H_
-#define NEURON_H_VERSION "0.2"
-#define NEURON_H_VERSION_LEN 4
-namespace neuronal { // neuron classes and functions
-     char[NEURON_H_VERSION_LEN] neuronal::neuron_version() {
-          return NEURON_H_VERSION;
-     }
-     class cell {
-          public:
-               int val[2];
-			int out;
-               int update(cell *input, int index) {
-				if(index == 1) {
-					this.val[0] = input->out;
-					return 0;
-				}
-				else if(index == 2) {
-					this.val[1] = input->out;
-					return 0;
-				}
-				else
-					return -1;
-			}
-			int calc(int method) {
-				if(method == 1)
-					this.out = val[1] || val[2];
-				else(method == 2)
-					this.out = val[1] && val[2];
-			}
-     };
-}
-namespace logic { // simple logics
-	class gates {
-		public:
-		int gates::xor(int bit1, int bit2) {
-			return (bit1 != bit2);
-		}
-		int gates::or(int bit1, int bit2) {
-			return (bit1 || bit2);
-		}
-		int gates::and(int bit1, int bit2) {
-			return  (bit1 && bit2);
-		}
-	};
+#define NEURON_H_VERSION 1
+int neuron(int bit1, int bit2, int level) {
+	if(level == 1)
+		return bit1 || bit2;
+	if(level == 2)
+		return bit1 && bit2;
+	else
+		return 0;
 }
 #endif
